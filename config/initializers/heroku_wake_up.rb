a3821144090212ab6371b55042e758dc80d66908
wesley rocha <wesleyweb@gmail.com>
2012-12-10 21:34:30 -0200

    atualizando heroku wake up e carrierwave

--- config/initializers/heroku_wake_up.rb
+++ config/initializers/heroku_wake_up.rb
@@ -2,9 +2,9 @@
 require 'rufus/scheduler'
 scheduler = Rufus::Scheduler.start_new
 
-scheduler.every '10m' do
-  require "net/http"
-  require "uri"
-  url = 'http://example.com'
-  Net::HTTP.get_response(URI.parse(url))
-end
+# scheduler.every '10m' do
+#   require "net/http"
+#   require "uri"
+#   url = 'http://example.com'
+#   Net::HTTP.get_response(URI.parse(url))
+# end
